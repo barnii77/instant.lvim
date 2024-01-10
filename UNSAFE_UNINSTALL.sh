980545fa74ccbf61b9261591fc6b58649a0d78dd
barnii77 <david.harrer@gmx.at>
2024-01-10 18:00:27 +0100

    added an unsafe uninstall script for testing purposes (WILL DESTROY YOUR CONSOLE CONFIG IF YOU HAVE ONE)

---
+++ UNSAFE_UNINSTALL.sh
@@ -0,0 +1,16 @@
+read -r -p "Are you sure you want to uninstall? [y/N] " response
+read -r -p "Are you really sure you want to uninstall? [y/N] " response
+read -r -p "Are you really really sure you want to uninstall? [y/N] " response
+read -r -p "Are you really really really sure (last time I will ask this) ? [y/N] " response
+
+if $HOME == "/home/david" || $HOME == "/home/david/" then
+  # stop the program so I dont accidentally run this on my own computer
+  echo "David you dummy dumb dumb what in the f*cking hell are you doing?!"
+  exit 1
+fi
+
+rm -rf $HOME/.config/lvim
+rm -rf $HOME/Downloads/instant.lvim
+bash $HOME/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
+rm $HOME/.bashrc
+rm $HOME/.profile
