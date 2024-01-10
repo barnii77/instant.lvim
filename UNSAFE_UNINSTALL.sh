d56677fb4d0f1a9a60b9861795f434a4afefe4a9
barnii77 <david.harrer@gmx.at>
2024-01-10 18:15:42 +0100

    chatgpt just fixed it

--- UNSAFE_UNINSTALL.sh
+++ UNSAFE_UNINSTALL.sh
@@ -3,13 +3,13 @@ read -r -p "Are you really sure you want to uninstall? [y/N] " response
 read -r -p "Are you really really sure you want to uninstall? [y/N] " response
 read -r -p "Are you really really really sure (last time I will ask this) ? [y/N] " response
 
-if $HOME == "/home/david" || $HOME == "/home/david/" then
+if [ "$HOME" = "/home/david" ] || [ "$HOME" = "/home/david/" ]; then
   # stop the program so I dont accidentally run this on my own computer
-  echo David you dummy dumb dumb what in the f*cking hell are you doing?!
+  echo "David you dummy dumb dumb what in the f*cking hell are you doing?!"
 else
-  rm -rf $HOME/.config/lvim
-  rm -rf $HOME/Downloads/instant.lvim
-  bash $HOME/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
-  rm $HOME/.bashrc
-  rm $HOME/.profile
+  rm -rf "$HOME/.config/lvim"
+  rm -rf "$HOME/Downloads/instant.lvim"
+  bash "$HOME/.local/share/lunarvim/lvim/utils/installer/uninstall.sh"
+  rm "$HOME/.bashrc"
+  rm "$HOME/.profile"
 fi
