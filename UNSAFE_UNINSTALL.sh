950d8aab62c7cdcf2084dc5b6a901285f28c9b15
barnii77 <david.harrer@gmx.at>
2024-01-10 18:11:09 +0100

    i dont quite understand whats wrong with the unsafe uninstall but whatever

--- UNSAFE_UNINSTALL.sh
+++ UNSAFE_UNINSTALL.sh
@@ -6,11 +6,10 @@ read -r -p "Are you really really really sure (last time I will ask this) ? [y/N
 if $HOME == "/home/david" || $HOME == "/home/david/" then
   # stop the program so I dont accidentally run this on my own computer
   echo "David you dummy dumb dumb what in the f*cking hell are you doing?!"
-  exit 1
+else
+  rm -rf $HOME/.config/lvim
+  rm -rf $HOME/Downloads/instant.lvim
+  bash $HOME/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
+  rm $HOME/.bashrc
+  rm $HOME/.profile
 fi
-
-rm -rf $HOME/.config/lvim
-rm -rf $HOME/Downloads/instant.lvim
-bash $HOME/.local/share/lunarvim/lvim/utils/installer/uninstall.sh
-rm $HOME/.bashrc
-rm $HOME/.profile
